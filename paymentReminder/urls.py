3713ed673070276e6279aabf41efdcac7255ad6e
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-03-20 18:26:38 +0200

    Add payments urls namespace

--- paymentReminder/urls.py
+++ paymentReminder/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from payments.views import HomeView
+from django.urls import path, include
 
 urlpatterns = [
-    path('', HomeView),
+    path('payments/', include('payments.urls', namespace='payments')),
     path('admin/', admin.site.urls),
 ]
