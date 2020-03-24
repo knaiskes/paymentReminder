ac65b2efd96d03584b1e27392fc5673d0cfead11
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-03-24 22:03:37 +0200

    add obligeds_list and obliged_by_id views and templates

--- payments/models.py
+++ payments/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
 class Obliged(models.Model):
     full_name = models.CharField(max_length=100, unique=True)
     email = models.EmailField(max_length=130, unique=True)
+
+    def get_obliged_url(self):
+        return reverse('payments:obliged_by_id', args=[self.id])
 
     def __str__(self):
         return self.full_name
--- payments/views.py
+++ payments/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Payment
+from .models import Payment, Obliged
 
 def payments_list(request):
     payments_list = Payment.objects.all()
@@ -15,3 +15,19 @@ def payment_by_id(request, id):
         'payment': payment,
     }
     return render(request, 'payments/payment.html', context)
+
+def obligeds_list(request):
+    obligeds_list = Obliged.objects.all()
+    context = {
+        'obligeds_list': obligeds_list,
+    }
+    return render(request, 'payments/obligeds.html', context)
+
+def obliged_by_id(request, id):
+    obliged = get_object_or_404(Obliged, id=id)
+    payments_list = Payment.objects.filter(obliged=id)
+    context = {
+        'obliged': obliged,
+        'payments_list': payments_list
+    }
+    return render(request, 'payments/obliged.html', context)
