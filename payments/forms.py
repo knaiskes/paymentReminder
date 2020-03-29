90028f4f09e32e34002b143df16d7c0003a15115
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-03-29 12:45:43 +0300

    Update DAYS_CHOICES values

--- payments/forms.py
+++ payments/forms.py
@@ -2,10 +2,10 @@ from django import forms
 
 DAYS_CHOICES = (
     ('', '-----'),
-    ('week', 'Week'),
-    ('month', 'Month'),
-    ('two_months', 'Two Months'),
-    ('three_months', 'Three Months'),
+    ('seven', '7 Days'),
+    ('thirty', '30 Days'),
+    ('sixty', '60 Days'),
+    ('ninety', '90 days'),
 )
 
 class DateHistorySearchForm(forms.Form):
